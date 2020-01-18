ad3911128245cbf42bbbd1fc703ddcf2ca6c3792
jonitt <jonit44@gmail.com>
2020-01-18 14:40:50 +0800

    Make list entries look better with more info

--- src/containers/restaurant-list/ListEntry.js
+++ src/containers/restaurant-list/ListEntry.js
@@ -3,7 +3,9 @@ import styles from './ListEntry.module.css';
 import {
   Grid,
   CardMedia,
-  Typography
+  Typography,
+  Card,
+  CardContent
 } from '@material-ui/core';
 
 export default class RestaurantList extends Component {
@@ -13,19 +15,53 @@ export default class RestaurantList extends Component {
     const { props } = this;
     const { restaurant } = props;
     return (
-      <Grid item className={styles.container}>
-        <CardMedia
-          classes={{ root: styles.image }}
-          image={restaurant.image}
-        >
-          <Typography
-            style={{ fontSize: '1.8rem' }}
-            align='right'
+      <Grid item>
+        <Card className={styles.container}>
+          <CardMedia
+            classes={{ root: styles.image }}
+            image={restaurant.image}
           >
-            {restaurant.city}
-          </Typography>
-        </CardMedia>{' '}
-        />
+            <Grid style={{height: '100%'}} container direction='row' justify='flex-end' alignItems='flex-end' alignContent='flex-end'>
+              <Grid item xs={2}>
+                <Typography
+                  style={{
+                    color: 'white',
+                    fontSize: '1.8rem',
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+                  }}
+                  align='right'
+                >
+                  {restaurant.city}
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <CardContent
+                  classes={{ root: styles.bottomContent }}
+                >
+                  <Typography
+                    style={{
+                      fontSize: '2.4rem',
+                      color: 'white'
+                    }}
+                    align='center'
+                  >
+                    {restaurant.name}
+                  </Typography>
+                  <Typography
+                    style={{
+                      fontSize: '1.8rem',
+                      color: 'white'
+                    }}
+                    align='center'
+                  >
+                    {restaurant.description}
+                  </Typography>
+                </CardContent>
+              </Grid>
+            </Grid>
+          </CardMedia>
+          />
+        </Card>
       </Grid>
     );
   }
