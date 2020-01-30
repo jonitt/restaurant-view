7cdf54c06592a9ef325ff4157ad4fb7b56e65001
jonitt <jonit44@gmail.com>
2020-01-31 01:56:31 +0800

    Added build

---
+++ build/precache-manifest.f61337092a71d06b4b762a8475851143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfaee98eb9682269f62f881a1d66e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05069006389e871e2801",
+    "url": "/static/css/main.6d58349e.chunk.css"
+  },
+  {
+    "revision": "b3ab080fa2f98c1ddd4e",
+    "url": "/static/js/2.957a915f.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.957a915f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05069006389e871e2801",
+    "url": "/static/js/main.c37df7ff.chunk.js"
+  },
+  {
+    "revision": "6f7155367504d444363c",
+    "url": "/static/js/runtime-main.18952408.js"
+  }
+]);
